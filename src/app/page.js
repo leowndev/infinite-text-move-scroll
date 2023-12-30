a4e645a878d5a0b1d0cac5499e42171e957a1977
Leonardo <leonardogt2308@gmail.com>
2023-12-30 17:01:37 -0300

    Display text at the bottom of background-image

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,13 @@ export default function Home() {
         src="/images/background.jpg"
         alt="image"
       />
+
+      <div className={styles.sliderContainer}>
+        <div className={styles.slider}>
+          <p>Infinite Text -</p>
+          <p>Infinite Text -</p>
+        </div>
+      </div>
     </main>
   )
 }
